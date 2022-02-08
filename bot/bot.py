41ebb6110ce221f8366407065a9ea4ed37d9bb96
blacktrub <lightlocdog@gmail.com>
2022-02-08 21:23:27 +0300

    add help command

--- bot/bot.py
+++ bot/bot.py
@@ -12,11 +12,23 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=["start", "help"])
+COMMANDS = {
+    "/start": "Start bot",
+    "/help": "Show this message",
+    "/add": "Add new project",
+    "/remove": "Remove project",
+}
+
+
+@dp.message_handler(commands=["start"])
 async def hello(msg: types.Message):
-    await msg.reply("Hello, chat!")
+    await msg.answer("Hello, chat!")
+
+
+@dp.message_handler(commands=["help"])
+async def help(msg: types.Message):
+    await msg.answer("\n".join((f"{k} - {v}" for k, v in COMMANDS.items())))
 
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-
